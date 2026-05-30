e47f75fc3a9b94864ff5eea5f1fd99aca1b25e8c
workes <bassdard2012@gmail.com>
2026-05-30 18:02:19 +0200

    fix attribute and tag key identity

--- Runtime/Attributes/AttributeKey.cs
+++ Runtime/Attributes/AttributeKey.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace com.workes.inventory.attributes
 {
-    public sealed class AttributeKey<T>
+    public sealed class AttributeKey<T> : IEquatable<AttributeKey<T>>
     {
         public string Id { get; }
 
@@ -12,6 +12,21 @@ namespace com.workes.inventory.attributes
                 throw new ArgumentException("AttributeKey id cannot be null or empty");
 
             Id = id;
+        }
+
+        public bool Equals(AttributeKey<T>? other)
+        {
+            return other != null && string.Equals(Id, other.Id, StringComparison.Ordinal);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is AttributeKey<T> other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(typeof(T), StringComparer.Ordinal.GetHashCode(Id));
         }
 
         public override string ToString() => Id;
--- Runtime/Tags/TagKey.cs
+++ Runtime/Tags/TagKey.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace com.workes.inventory.tags
 {
-    public sealed class TagKey
+    public sealed class TagKey : IEquatable<TagKey>
     {
         public string Id { get; }
 
@@ -12,6 +12,21 @@ namespace com.workes.inventory.tags
                 throw new ArgumentException("TagKey id cannot be null or empty.");
 
             Id = id;
+        }
+
+        public bool Equals(TagKey? other)
+        {
+            return other != null && string.Equals(Id, other.Id, StringComparison.Ordinal);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is TagKey other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return StringComparer.Ordinal.GetHashCode(Id);
         }
 
         public override string ToString() => Id;
